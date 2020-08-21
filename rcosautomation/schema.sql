b79f5089bb7333a983fbf8319fff9e24f371b8b8
Frank Matranga <thefrankmatranga@gmail.com>
2020-08-21 13:18:10 -0400

    Add SQLITE support

---
+++ rcosautomation/schema.sql
@@ -0,0 +1,4 @@
+CREATE TABLE IF NOT EXISTS users (
+	rcs_id TEXT NOT NULL PRIMARY KEY,
+	discord_user_id TEXT NOT NULL UNIQUE
+)
--- rcosautomation/views.py
+++ rcosautomation/views.py
@@ -1,7 +1,9 @@
 import os
-from flask import Flask, session, request, render_template, redirect
+import sqlite3
+from flask import Flask, g, session, request, render_template, redirect
 from flask_cas import CAS, login_required, logout
 from .discord import get_tokens, get_user_info, add_user_to_server, RCOS_SERVER_ID, DISCORD_REDIRECT_URL
+from .db import query_db, get_db
 
 app = Flask(__name__)
 cas = CAS(app, '/cas')
@@ -15,10 +17,15 @@ app.config['CAS_AFTER_LOGIN'] = '/'
 @login_required
 def join():
     if request.method == 'GET':
-        return render_template(
-            'join.html',
-            rcs_id=cas.username.lower()
-        )
+        row = query_db("SELECT discord_user_id FROM users WHERE rcs_id=?", (cas.username.lower(),), True)
+        
+        if row == None:
+            return render_template(
+                'join.html',
+                rcs_id=cas.username.lower()
+            )
+        else:
+            return render_template('already_joined.html', rcs_id=cas.username.lower())
     elif request.method == 'POST':
         session['user_info'] = {
             'first_name': request.form['first_name'].strip(),
@@ -48,7 +55,28 @@ def discord_callback():
     grad_year_digits = str(session['user_info']['graduation_year'])[2:]
     nickname = f'{name} \'{grad_year_digits} ({cas.username.lower()})'
 
+    # Add to database
+    try:
+        c = get_db().cursor()
+        c.execute('INSERT INTO users VALUES (?, ?)', (cas.username.lower(), user['id']))
+        get_db().commit()
+    except sqlite3.IntegrityError:
+        return render_template('already_joined.html', rcs_id=cas.username.lower())
+
     # Add user to server
     add_user_to_server(tokens['access_token'], user['id'], nickname)
 
     return render_template('done.html', nickname=nickname, discord_server_id=RCOS_SERVER_ID)
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+def init_db():
+    with app.app_context():
+        db = get_db()
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
