b7f613a45070a11b9ddd79e8228b19fb328f81a1
Frank Matranga <thefrankmatranga@gmail.com>
2020-08-27 19:32:37 -0400

    Handle exceptions in delete channels script

--- scripts/delete_channels.py
+++ scripts/delete_channels.py
@@ -38,7 +38,12 @@ def delete_channel(channel_id: str):
 
 while True:
     channel_id = input('What is the ID of the channel you want to delete?\n>')
-    channel = get_channel(channel_id)
+
+    try:
+        channel = get_channel(channel_id)
+    except:
+        print(f'Failed to find channel with ID {channel_id}')
+        continue
 
     delete_count = 0
 
@@ -50,15 +55,23 @@ while True:
         
         if delete_children == 'Y':
             print('Deleting children')
-            children = get_category_children(channel_id)
-            for child in children:
-                delete_channel(child['id'])
-                delete_count += 1
+            try:
+                children = get_category_children(channel_id)
+                for child in children:
+                    try:
+                        delete_channel(child['id'])
+                        delete_count += 1
+                    except:
+                        print(f'Failed to delete child channel `{child["name"]}`')
+            except:
+                print('Failed to find children channels')
         else:
             print('Not deleting children channels')
 
     # Delete channel
-    delete_channel(channel_id)
-    delete_count += 1
-
-    print(f'Deleted `{channel["name"]}` ({delete_count} channels total)')
+    try:
+        delete_channel(channel_id)
+        delete_count += 1
+        print(f'Deleted `{channel["name"]}` ({delete_count} channels total)')
+    except:
+        print(f'Failed to delete `{channel["name"]}`. Deleted {delete_count} channels total')
