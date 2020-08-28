f609d320fdb6a024a762590f9ff97b2297aabdd0
Frank Matranga <thefrankmatranga@gmail.com>
2020-08-28 15:33:49 -0400

    Remove session

--- rcosautomation/views.py
+++ rcosautomation/views.py
@@ -1,4 +1,5 @@
 import os
+import traceback
 from flask import Flask, g, session, request, render_template, redirect
 from flask_cas import CAS, login_required, logout
 from werkzeug.exceptions import HTTPException
@@ -20,13 +21,13 @@ app.config['CAS_AFTER_LOGIN'] = '/'
 @app.route('/', methods=['GET', 'POST'])
 @login_required
 def join():
-    
     if request.method == 'GET':
         user = mongo.db.users.find_one({'rcs_id': cas.username.lower()})
         if user == None:
-            user = mongo.db.users.insert_one({ 'rcs_id': cas.username.lower(), 'name': {}, 'graduation_year': 2020 })
-
-        if user['discord'] == None:
+            mongo.db.users.insert_one({ 'rcs_id': cas.username.lower(), 'name': {}, 'graduation_year': 2020 })
+            user = mongo.db.users.find_one({'rcs_id': cas.username.lower()})
+            print('Created ', user)
+        if 'discord' not in user:
             return render_template(
                 'join.html',
                 user=user,
@@ -51,13 +52,6 @@ def join():
 
         print(f'{cas.username} is starting to connect their Discord account with the identity {first_name} {last_name} \'{graduation_year}')
 
-        session['user_info'] = {
-            'first_name': first_name,
-            'last_name': last_name,
-            'graduation_year': graduation_year
-        }
-        session.modified = True
-
         user_data = {
             'rcs_id': cas.username.lower(),
             'name': {'first': first_name, 'last': last_name},
@@ -75,7 +69,7 @@ def join():
 @login_required
 def connected():
     user = mongo.db.users.find_one({'rcs_id': cas.username.lower()})
-    if user == None:
+    if user == None or 'discord' not in user:
         return redirect('/')
 
     return render_template('connected.html', user=user, discord_server_id=RCOS_SERVER_ID)
@@ -99,26 +93,28 @@ def discord_callback():
     session['tokens'] = tokens
 
     # Get user id
-    user = get_user_info(tokens['access_token'])
-
+    discord_user = get_user_info(tokens['access_token'])
+    
+    user = mongo.db.users.find_one({'rcs_id': cas.username.lower()})
     # Generate Discord nickname to set as "<First Name> <Last Name Initial> '<Graduation Year 2 Digits> (<RCS ID>)"
-    name = session['user_info']['first_name'] + ' ' + \
-        session['user_info']['last_name'][0].upper()
-    grad_year_digits = str(session['user_info']['graduation_year'])[2:]
+    
+    
+    name = user['name']['first'] + ' ' + user['name']['last'][0].upper()
+    grad_year_digits = str(user['graduation_year'])[2:]
     nickname = f'{name} \'{grad_year_digits} ({cas.username.lower()})'
 
     # Add to database
-    mongo.db.users.update_one({'rcs_id': cas.username.lower()}, {'$set': {
+    mongo.db.users.find_one_and_update({'rcs_id': cas.username.lower()}, {'$set': {
         'discord': {
             'tokens': tokens,
-            'user_id': user['id']
+            'user_id': discord_user['id']
         }
     }})
     print(f'Added DB record for {cas.username}')
 
     # Add user to server
-    add_user_to_server(tokens['access_token'], user['id'], nickname)
-    print(f'Added {cas.username} to the server as {nickname}')
+    add_user_to_server(tokens['access_token'], discord_user['id'], nickname)
+    print(f'Added {cas.username.lower()} to the server as {nickname}')
 
     return redirect('/connected')
 
@@ -137,6 +133,7 @@ def discord_reset():
 @app.errorhandler(Exception)
 def handle_error(e):
     print(e)
+    print(traceback.format_exc())
 
     # Hide error in production
     error = e
